9d310ac0c396b4faae47145310a572f235746a60
bayegorguipene41-prog <bayegorguipene41@gmail.com>
2026-08-29 17:08:13 +0200

    Correggi configurazione per deploy Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+process.env.NEXT_TELEMETRY_DISABLED = '1'
+
 const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
+  // turbopack: {
+  //   root: __dirname,
+  // },
 }
 
 module.exports = nextConfig

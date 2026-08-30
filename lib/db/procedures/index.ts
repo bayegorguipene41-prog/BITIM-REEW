a08fe85d61ea0555a6902c8285acdde35951ea1b
bayegorguipene41-prog <bayegorguipene41@gmail.com>
2026-08-30 03:18:29 +0200

    Allinea nomi file e aggiungi procedure dettagliate Italia

--- lib/db/procedures/index.ts
+++ lib/db/procedures/index.ts
@@ -1,23 +1,26 @@
 // 📌 IMPORTA TUTTE LE PROCEDURE
-import { procedureItalia } from "./Italia";
+import { procedureItalia } from "./italia";
 import { procedureFrancia } from "./francia";
-import { procedureGermania } from "./Germania";
-import { procedureSpagna } from "./Spagna";
-import { procedureRegnoUnito } from "./Regno Unito";
-import { procedureAustria } from "./Austria";
+import { procedureGermania } from "./germania";
+import { procedureSpagna } from "./spagna";
+import { procedureRegnoUnito } from "./RegnoUnito";
+import { procedureAustria } from "./austria";
 import { procedureBelgio } from "./Belgio";
-import { procedureSvizzera } from "./Svizzera";
+import { procedureSvizzera } from "./svizzera";
 import { procedurePortogallo } from "./Portogallo";
 import { procedureIrlanda } from "./Irlanda";
-import { procedureSvezia } from "./Svezia";
+import { procedureSvezia } from "./svezia";
 import { procedureStatiUniti } from "./StatiUniti";
 import { procedureMarocco } from "./Marocco";
 import { procedureAlgeria } from "./Algeria";
-import { procedureTunisia } from "./Tunisia";
-import { procedureSenegal } from "./Senegal";
-import { procedureCina } from "./Cina";
+import { procedureTunisia } from "./tunisia";
+import { procedureSenegal } from "./senegal";
+import { procedureCina } from "./cina";
 import { procedureMali } from "./Mali";
 import { procedurePaesiBassi } from "./PaesiBassi";
+
+// ✅ NUOVA: Importa le procedure dettagliate dell'Italia
+import { PROCEDURE_ITALIA } from "./italia";
 
 // 📌 ESPORTA TUTTE INSIEME
 export const PROCEDURES = [
@@ -40,4 +43,7 @@ export const PROCEDURES = [
   procedureCina,
   procedureMali,
   procedurePaesiBassi,
+
+  // ✅ Aggiungi qui le procedure dettagliate nuove
+  ...PROCEDURE_ITALIA,
 ];
